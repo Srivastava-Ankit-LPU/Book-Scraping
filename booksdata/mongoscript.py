1ac366e5bae30631cfbcf78f44f56b4aeb129833
Srivastava-Ankit-LPU <83294365+Srivastava-Ankit-LPU@users.noreply.github.com>
2024-02-15 10:53:08 +0530

    Add files via upload

---
+++ booksdata/mongoscript.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+import datetime
+client = MongoClient("mongodb+srv://ankshrivastava17:ankit1234@cluster0.tog3llt.mongodb.net/")
+
+
+db=client.scrapy
+posts=db.test_collection
+doc=post = {
+    "author": "Mike",
+    "text": "My first blog post!",
+    "tags": ["mongodb", "python", "pymongo"],
+    "date": datetime.datetime.now(tz=datetime.timezone.utc),
+}
+
+post_id = posts.insert_one(post).inserted_id
+print(post_id)
